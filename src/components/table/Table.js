6ad79469d36620e3ec50459b48002d76624be216
XMnikita <Xmnikita.01@mail.ru>
2021-09-09 11:00:56 +0300

    complite excell table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,33 +1,29 @@
 import { ExcelComponent } from '../../core/ExcelComponent'
+import { createTable } from './table.template'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
-  toHTML() {
-    return `
-          <div class="row">
-            <div class="row-info"></div>
-            <div class="row-data">
-              <div class="column">A</div>
-              <div class="column">B</div>
-              <div class="column">C</div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="row-info">1</div>
-            <div class="row-data">
-              <div class="cell selected" contenteditable="true">123</div>
-              <div class="cell" contenteditable="true">345</div>
-              <div class="cell" contenteditable="true">sdfs</div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="row-info">2</div>
-            <div class="row-data">
-              <div class="cell" contenteditable="true">123</div>
-              <div class="cell" contenteditable="true">345</div>
-              <div class="cell" contenteditable="true">sdfs</div>
-            </div>
-          </div>
-    `
+
+  constructor($root) {
+    super($root, {
+      name: 'Table',
+      // listeners: ['click', 'mousedown', 'mouseup', 'mousemove'],
+    })
   }
+
+  toHTML() {
+    return createTable(20)
+  }
+
+  onClick() {
+    console.log('table Click')
+  }
+
+  onMousedown() {
+    console.log('table mouse Down')
+  }
+
+  onMousemove() {}
+
+  onMouseup() {}
 }
