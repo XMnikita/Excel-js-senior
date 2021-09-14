f0f904dabfb39733966ef9c5d159addc9757ed28
XMnikita <Xmnikita.01@mail.ru>
2021-09-14 17:25:37 +0300

    add Observ Pattern (add class Emiter)

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,6 +5,7 @@ export class ExcelComponent extends DomListener {
     super($root, options.listeners)
     this.name = options.name
   }
+
   // Возвращает шаблон элемента
   toHTML() {
     return ''
